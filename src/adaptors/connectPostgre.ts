2af151bc9bac02e278e89d7102956a72a77e8977
Vladyslav Proshchavaiev <f3joule@gmail.com>
2019-09-25 21:05:15 +0300

    Write Postgres' host and port to console on connect

--- src/adaptors/connectPostgre.ts
+++ src/adaptors/connectPostgre.ts
@@ -2,10 +2,18 @@ const { Pool } = require('pg')
 
 require("dotenv").config();
 
-export const pool = new Pool({
+const pgConf = {
   user: process.env.PGUSER,
   host: process.env.PGHOST,
   database: process.env.PGDATABASE,
   password: process.env.PGPASSWORD,
   port: process.env.PGPORT,
-});
+};
+
+let greeted = false
+if (!greeted) {
+  greeted = true;
+  console.log(`Connecting to Postgres at ${pgConf.host}:${pgConf.port} as user '${pgConf.user}'`);
+}
+
+export const pool = new Pool(pgConf);
