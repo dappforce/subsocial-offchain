7074b069fde3723adfd5d5d3ba2f715f0ffd0c92
Vladyslav Proshchavaiev <f3joule@gmail.com>
2019-10-21 13:56:10 +0200

    Add check 'if not exist' to prevent errors

--- src/search/index.ts
+++ src/search/index.ts
@@ -1,21 +1,28 @@
 import searchClient from '../adaptors/connectElasticsearch'
-import * as BlogsMapping from './mappings/blogs.json' 
-import * as PostsMapping from './mappings/posts.json' 
-import * as CommentsMapping from './mappings/comments.json' 
-import * as ProfilesMapping from './mappings/profiles.json' 
+import * as BlogsMapping from './mappings/blogs.json'
+import * as PostsMapping from './mappings/posts.json'
+import * as CommentsMapping from './mappings/comments.json'
+import * as ProfilesMapping from './mappings/profiles.json'
 
 async function run() {
-  await insertSearchIndex('subsocial_blogs', BlogsMapping)
-  await insertSearchIndex('subsocial_posts', PostsMapping)
-  await insertSearchIndex('subsocial_comments', CommentsMapping)
-  await insertSearchIndex('subsocial_profiles', ProfilesMapping)
+  await insertSearchIndexIfNotExist('subsocial_blogs', BlogsMapping)
+  await insertSearchIndexIfNotExist('subsocial_posts', PostsMapping)
+  await insertSearchIndexIfNotExist('subsocial_comments', CommentsMapping)
+  await insertSearchIndexIfNotExist('subsocial_profiles', ProfilesMapping)
 }
 
-async function insertSearchIndex(name: string, body: any) {
-  await searchClient.indices.create({
-    index: name,
-    body: body
-  })
+async function insertSearchIndexIfNotExist(name: string, body: any) {
+  const result = await searchClient.indices.exists({
+    index: name
+  }, { ignore: [404] })
+
+  if (result.statusCode == 404) {
+    await searchClient.indices.create({
+      index: name,
+      body: body
+    })
+    console.log('\'' + name + '\' index created')
+  } else console.log('\'' + name + '\' index already exists')
 }
 
 run().catch(console.log)
