8140501ff2a4d2043dffa65ff1e0959ba4d61b3b
Vladyslav Proshchavaiev <f3joule@gmail.com>
2019-11-21 15:07:09 +0200

    Update Dockerfile and docker-compose
    - Now path in sql/init.ts depends on NODE_ENV. When production it looks through .sql in build only
    - Add static sql files copying as yarn script

--- src/sql/init.ts
+++ src/sql/init.ts
@@ -1,8 +1,10 @@
 import { readFileSync } from 'fs';
 import { pool } from './../adaptors/connectPostgre';
 
-const initSchema = readFileSync('src/sql/initSchema.sql', 'utf8'); 
-const initDb = readFileSync('src/sql/initDb.sql', 'utf8'); 
+const ENV = process.env.NODE_ENV || 'development';
+const buildPath = ENV === 'production' ? 'build/' : '';
+const initSchema = readFileSync(buildPath + 'src/sql/initSchema.sql', 'utf8'); 
+const initDb = readFileSync(buildPath + 'src/sql/initDb.sql', 'utf8'); 
 
 pool.query(initSchema,(err) => {
     if (err) throw new Error(err);
