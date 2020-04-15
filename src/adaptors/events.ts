90ee82e786e37a034fef0556561553d84ce906cd
Alex <sagaidak1337@gmail.com>
2020-04-15 20:06:59 +0300

    after rebase

--- src/adaptors/events.ts
+++ src/adaptors/events.ts
@@ -1,5 +1,5 @@
 import * as events from 'events'
-import { EventData } from '@polkadot/types/type/Event';
+import { EventData } from '@polkadot/types/generic/Event';
 import BN from 'bn.js';
 
 export const eventEmitter = new events.EventEmitter();
