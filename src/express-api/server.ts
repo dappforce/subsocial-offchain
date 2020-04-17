4fa8f1ac86a48135f845e0333da33c679c381100
Alex Siman <aleksandr.siman@gmail.com>
2020-04-18 00:29:38 +0300

    Extract notificaions web socket to a separate file

--- src/express-api/server.ts
+++ src/express-api/server.ts
@@ -1,4 +1,3 @@
-import * as WebSocket from 'ws';
 import * as express from 'express'
 import * as bodyParser from 'body-parser'
 import * as cors from 'cors';
@@ -7,13 +6,13 @@ import { pool } from '../adaptors/connect-postgres';
 import { logSuccess, logError } from '../postgres/postges-logger';
 import { newLogger } from '@subsocial/utils';
 import { parseSiteWithRequest as siteParser } from '../parser/parse-site'
-import { eventEmitter, EVENT_UPDATE_NOTIFICATIONS_COUNTER } from '../adaptors/events';
-import { getUnreadNotifications } from '../postgres/notifications';
+import { eventEmitter, EVENT_UPDATE_NOTIFICATIONS_COUNTER } from './events';
+import { startNotificationsServer } from './ws';
 
 // import * as multer from 'multer';
 // const upload = multer();
 
-require("dotenv").config();
+require('dotenv').config();
 const LIMIT = process.env.PGLIMIT || '20';
 
 const log = newLogger('ExpressOffchainApi')
@@ -22,7 +21,6 @@ const app = express();
 app.use(cors());
 
 const fileSizeLimit = process.env.IPFS_MAX_FILE_SIZE
-
 
 // for parsing application/json
 app.use(bodyParser.json({ limit: fileSizeLimit }));
@@ -106,6 +104,7 @@ app.get('/v1/offchain/notifications/:id', async (req: express.Request, res: expr
 
   }
 });
+
 app.post('/v1/offchain/notifications/:id/readAll', async (req: express.Request, res: express.Response) => {
   const account = req.params.id;
   log.info(`Mark all notifications as read by account: ${account}`)
@@ -128,54 +127,19 @@ app.post('/v1/offchain/notifications/:id/readAll', async (req: express.Request, 
     res.json(data.rows);
   } catch (err) {
     logError('mark all notifications as read', `by account: ${account}`, err.stack);
-
   }
 });
 
+// TODO Rename to '/v1/parseSite'
 app.post('/offchain/parser/', async (req: express.Request, res: express.Response) => {
   const data = await siteParser(req.body.url)
 
   res.send(data);
 });
 
-const wsPort = parseInt(process.env.OFFCHAIN_WS_PORT || '3011')
-const wss = new WebSocket.Server({ port: wsPort });
-
-const clients: any = {}
-
-wss.on('connection', (ws: WebSocket) => {
-
-  ws.on('message', async (account: string) => {
-    log.debug('Notifications web socket: Received a message from account:', account);
-
-    const currentUnreadCount = await getUnreadNotifications(account)
-
-    clients[account] = ws;
-    clients[account].send(`${currentUnreadCount}`)
-  });
-
-  eventEmitter.on(EVENT_UPDATE_NOTIFICATIONS_COUNTER, (account: string, currentUnreadCount: number) => {
-    if (!clients[account]) return
-    if (clients[account].readyState !== WebSocket.OPEN) {
-      delete clients[account]
-      return
-    }
-    log.debug(`Notifications web socket: Message sent to account: ${account}`)
-
-    clients[account].send(`${currentUnreadCount}`)
-  })
-
-  ws.on('close', (ws: WebSocket) => {
-    log.info(`Disconnected Notifications Counter Web Socket by id: ${ws}`);
-  });
-});
-
-wss.on('close', () => {
-  log.info('Disconnected Notifications Counter Web Socket Server');
-});
+startNotificationsServer()
 
 const port = process.env.OFFCHAIN_SERVER_PORT
 app.listen(port, () => {
   log.info(`HTTP server started on port ${port}`)
-
 })
