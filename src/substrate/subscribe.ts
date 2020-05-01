ef748013db998cf28dc1a74af88cec8456958a80
Alex Siman <aleksandr.siman@gmail.com>
2020-05-01 09:56:12 +0300

    Rewrite the logic of finding a min block to process

--- src/substrate/subscribe.ts
+++ src/substrate/subscribe.ts
@@ -1,67 +1,72 @@
-import { BlockNumber, Hash, Event } from '@polkadot/types/interfaces';
 import { SubsocialSubstrateApi } from '@subsocial/api/substrate';
 import { Api } from '@subsocial/api/substrateConnect';
 import { substrateLog as log } from '../connections/loggers';
-import { eventsFilterMethods, eventsFilterSections } from './utils';
+import { shouldHandleEvent, eventsFilterMethods } from './utils';
 import { readOffchainState, writeOffchainState } from './offchain-state';
 import { handleEventForElastic } from './handle-elastic';
 import { handleEventForPostgres } from './handle-postgres';
 
-require('dotenv').config();
+require('dotenv').config()
 
-export let substrate: SubsocialSubstrateApi;
-
-function shouldHandleEvent (event: Event): boolean {
-  return (
-    eventsFilterSections.includes(event.section.toString()) && 
-    eventsFilterMethods.includes(event.method.toString())
-  ) || eventsFilterSections.includes('all')
-}
+export let substrate: SubsocialSubstrateApi
 
 async function main () {
-  log.info(`Subscribe to Substrate events: ${eventsFilterMethods}`);
+  log.info(`Subscribe to Substrate events: ${Array.from(eventsFilterMethods)}`)
 
-  // Connect to Substrate node:
+  // Connect to Subsocial's Substrate node:
   const api = await Api.connect(process.env.SUBSTRATE_URL)
-  substrate = new SubsocialSubstrateApi(api);
+  substrate = new SubsocialSubstrateApi(api)
 
-  const getBestFinalizedBlock = async () =>
-    (await api.derive.chain.bestNumberFinalized())
-
-  const blockTime = api.consts.timestamp?.minimumPeriod.muln(2).toNumber();
+  const blockTime = api.consts.timestamp?.minimumPeriod.muln(2).toNumber()
 
   const waitNextBlock = async () =>
-    new Promise(r => setTimeout(r, blockTime))
+    new Promise(resolve => setTimeout(resolve, blockTime))
 
   const state = await readOffchainState()
+  // Clean up the state from the last errors:
+  delete state.postgres.lastError
+  delete state.elastic.lastError
 
   const lastPostgresBlock = () => state.postgres.lastBlock
   const lastElasticBlock = () => state.elastic.lastBlock
 
-  let bestFinalizedBlock = await getBestFinalizedBlock()
+  const lastPostgresError = () => state.postgres.lastError
+  const lastElasticError = () => state.elastic.lastError
+
+  // Set default vals:
+  let processPostgres = true
+  let processElastic = true
+  let lastBlock = 0
   let blockToProcess = 0
-  let processPostgres = false
-  let processElastic = false
+
+  const getBestFinalizedBlock = async () =>
+    await api.derive.chain.bestNumberFinalized()
+
+  let bestFinalizedBlock = await getBestFinalizedBlock()
 
   while (true) {
 
-    // Doesn't matter if both Postgres and Elastic have the same last block number:
-    blockToProcess = lastPostgresBlock()
-    processPostgres = false
-    processElastic = false
-    
-    if (lastElasticBlock() < lastPostgresBlock()) {
-      blockToProcess = lastElasticBlock()
-      processElastic = true
-    } else if (lastPostgresBlock() < lastElasticBlock()) {
-      blockToProcess = lastPostgresBlock()
-      processPostgres = true
-    } else {
-      processPostgres = true
-      processElastic = true
+    if (lastPostgresError() && lastElasticError()) {
+      log.warn('Both Postgres and Elastic event handlers returned errors. Cannot continue event processing')
+      break
     }
 
-    blockToProcess++
+    // Reset processing flags:
+    processPostgres = !lastPostgresError()
+    processElastic = !lastElasticError()
+    
+    // Doesn't matter if both Postgres and Elastic have the same last block number:
+    lastBlock = lastPostgresBlock()
+
+    if (processPostgres && (!processElastic || lastPostgresBlock() < lastElasticBlock())) {
+      lastBlock = lastPostgresBlock()
+      processElastic = false
+    } else if (processElastic && (!processPostgres || lastElasticBlock() < lastPostgresBlock())) {
+      lastBlock = lastElasticBlock()
+      processPostgres = false
+    }
+
+    blockToProcess = lastBlock + 1
 
     if (bestFinalizedBlock.toNumber() < blockToProcess) {
       log.debug('Waiting for the best finalized block...')
@@ -70,8 +75,8 @@ async function main () {
       continue
     }
 
-    const blockNumber: BlockNumber = api.createType('BlockNumber', blockToProcess)
-    const blockHash: Hash = await api.rpc.chain.getBlockHash(blockNumber)
+    const blockNumber = api.createType('BlockNumber', blockToProcess)
+    const blockHash = await api.rpc.chain.getBlockHash(blockNumber)
     const events = await api.query.system.events.at(blockHash)
 
     log.debug(`Best finalized block: ${bestFinalizedBlock.toString()}`)
@@ -81,6 +86,7 @@ async function main () {
     for (const { event } of events) {
       if (shouldHandleEvent(event)) {
         log.debug(`Handle a new event: %o`, event.data)
+
         const eventMeta = {
           eventName: event.method,
           data: event.data,
@@ -108,25 +114,18 @@ async function main () {
     }
 
     if (processPostgres) {
-      delete state.postgres.lastError
       state.postgres.lastBlock = blockToProcess
     }
 
     if (processElastic) {
-      delete state.elastic.lastError
       state.elastic.lastBlock = blockToProcess
     }
 
     await writeOffchainState(state)
-
-    if (!processPostgres && !processElastic) {
-      log.warn('Both Postgres and Elastic event handlers returned errors. Cannot continue block processing')
-      break
-    }
   }
 }
 
 main().catch((error) => {
-  log.error('Failed to subscribe to events', error);
-  process.exit(-1);
-});
+  log.error('Failed to subscribe to events:', error)
+  process.exit(-1)
+})
