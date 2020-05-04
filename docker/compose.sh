01f9856488ee27038554ed3114154ad55d022dbf
Vladyslav Proshchavaiev <f3joule@gmail.com>
2020-05-04 20:07:10 +0300

    Start IPFS Cluster instead of simple daemon
    Make compose.sh more complex
    Expose notifications WebSocket port
    Fix .gitignore

--- docker/compose.sh
+++ docker/compose.sh
@@ -3,35 +3,55 @@ set -e
 
 pushd . > /dev/null
 
-# The following line ensure we run from the docker folder
-PROJECT_ROOT=`git rev-parse --show-toplevel`
-cd $PROJECT_ROOT/docker
+# The following line ensure we run from the script folder
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+eval cd $DIR
 
-if [[ $1 = 'down' ]]
-then
-  time docker-compose down;
-  exit 0;
-fi
+UTILS_ONLY=false
 
-POSTGRES_SERVICE=postgres
-ES_SERVICE=elasticsearch
-OFFCHAIN_SERVICE=subsocial-offchain
+case $1 in
+  --down)
+    time docker-compose kill
+    if [[ $2 == 'clean' ]]; then
+      echo "Cleaning volumes..."
+      eval docker-compose down
+      echo "Cleaning data directory..."
+      sudo rm -rf data
+    fi
+    eval sleep 2 && clear
+    exit 0
+    ;;
+  --utils)
+    UTILS_ONLY=true
+    ;;
+  *)
+    printf "Invalid argument provided.\n\nExamples:\n"
+    printf "Start all:\n./compose.sh\n\n"
+    printf "Start services without offchain itself:\n./compose.sh --utils\n\n"
+    printf "Stop all:\n./compose.sh --down\n\n"
+    printf "Clean all:\n./compose.sh --down clean\n"
+    exit 1
+    ;;
+esac
 
-ES_NODE_URL=$(grep ES_NODE_URL ../.env | xargs)
-ES_NODE_URL=${ES_NODE_URL#*=}
+UTILS=" postgres elasticsearch cluster"
+
+ES_NODE_URL='http://127.0.0.1:9200'
 
 time (
   echo "Starting offchain in background, hang on!"
 
-  docker-compose up -d $POSTGRES_SERVICE
-  docker-compose up -d $ES_SERVICE
+  if [ UTILS_ONLY ]; then
+    eval docker-compose up -d $UTILS
+  else
+    eval docker-compose up -d
 
-  echo "Elasticsearch is starting..."
-  until curl -s $ES_NODE_URL > /dev/null; do
-    sleep 2
-  done
-
-  docker-compose up -d $OFFCHAIN_SERVICE
+    echo "Starting Elasticsearch..."
+    until curl -s $ES_NODE_URL > /dev/null; do
+      sleep 2
+    done
+    eval docker restart subsocial-offchain
+  fi
 )
 
 echo "Containers are ready."
