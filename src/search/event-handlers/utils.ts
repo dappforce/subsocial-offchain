8ea67890a58a463aada63096b0216f428b8a6bb6
Alex Siman <aleksandr.siman@gmail.com>
2020-09-21 16:55:38 +0300

    Update src/search/event-handlers/utils.ts

--- src/search/event-handlers/utils.ts
+++ src/search/event-handlers/utils.ts
@@ -4,5 +4,7 @@ import { indexContentFromIpfs } from '../indexer';
 import { resolveCidOfContent } from '@subsocial/api/utils';
 
 export const indexSpaceContent = (space: Space) => indexContentFromIpfs(ES_INDEX_SPACES, resolveCidOfContent(space.content), space.id)
+
 export const indexProfileContent = (profile: Profile) => indexContentFromIpfs(ES_INDEX_PROFILES, resolveCidOfContent(profile.content), profile.created.account);
+
 export const indexPostContent = (post: Post) => indexContentFromIpfs(ES_INDEX_POSTS, resolveCidOfContent(post.content), post.id, post)
