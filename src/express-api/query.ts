ffafc032b616ae5499b4e658d735bf59cf252c62
olehmell <oleh.melnechyk@gmail.com>
2020-10-16 22:00:04 +0300

    Refactor query

--- src/express-api/query.ts
+++ src/express-api/query.ts
@@ -1,69 +1,66 @@
-import { newLogger, isDef } from "@subsocial/utils"
+import { newLogger } from "@subsocial/utils"
 import { pg } from '../connections/connect-postgres'
-import { logSuccess, logError } from '../postgres/postges-logger'
 import { Activity } from '@subsocial/types'
 
 const log = newLogger('ExpressOffchainQuery')
 
 type ActivitiesParams = {
   account: string,
-  offset?: number,
-  limit?: number
+  offset: number,
+  limit: number
 }
 
 type Table = 'news_feed' | 'notifications'
 
-const getActivitiesOrCountFrom = async <T extends Activity[] | number>(
-    table: Table,
-    what: 'data' | 'count',
-    { account, offset, limit }: ActivitiesParams
-  ): Promise<T> => {
-  const isCountQuery = what === 'count'
-  const queryData = isCountQuery ? 'COUNT(*)' : 'DISTINCT *'
-  let otherQueryParts = isCountQuery ? '' : 'ORDER BY date DESC'
-  const details = table === 'notifications' ? 'AND aggregated = true' : ''
-
-  if (isDef(offset)) {
-    otherQueryParts += '\nOFFSET $2'
-  }
-
-  if (isDef(limit)) {
-    otherQueryParts += '\nLIMIT $3'
-  }
-
-  const query = `
-    SELECT ${queryData} 
+const createActivityQuery = (table: Table) => 
+  `SELECT DISTINCT * 
     FROM df.activities
     WHERE id IN (
       SELECT activity_id
       FROM df.${table}
-      WHERE account = $1 ${details}
+      WHERE account = $1 AND aggregated = true
     )
-    ${otherQueryParts}`;
-  
-  const params = [ account, offset, limit ].filter(isDef);
-  log.debug(`SQL params: ${params}`);
+    ORDER BY date DESC
+    OFFSET $2
+    LIMIT $3`
 
-  const msg = `${what} from ${table}`
+const createCountQuery = (table: Table) => 
+  `SELECT COUNT(*)
+    FROM df.activities
+    WHERE id IN (
+      SELECT activity_id
+      FROM df.${table}
+      WHERE account = $1
+    )`
 
+const getQuery = async (
+    query: string,
+    params: any[],
+    errorMsg: string
+  ): Promise<any> => {
   try {
     const data = await pg.query(query, params)
-    logSuccess(msg, `by account: ${account}`)
-
-    return isCountQuery ? data.rows.pop().count : data.rows as T;
+    return data
   } catch (err) {
-    logError(msg, `by account: ${account}`, err.stack);
+    log.error(errorMsg, err.stack);
     throw err.stack
   }
 }
 
-const getActivitiesFrom = (from: Table, params: ActivitiesParams) => getActivitiesOrCountFrom<Activity[]>(from, 'data', params)
-const getCountFrom = (from: Table, params: ActivitiesParams) => getActivitiesOrCountFrom<number>(from, 'count', params)
+const getActivitiesFrom = (table: Table, { account, offset, limit }: ActivitiesParams): Promise<Activity[]> => getQuery(
+  createActivityQuery(table),
+  [ account, offset, limit ],
+  `Failed load to activities from ${table} by account ${account}`)
+
+const getCountFrom = (table: Table, account: string) => getQuery(
+  createCountQuery(table),
+  [ account ],
+  `Failed load to activities from ${table} by account ${account}`)
 
 export type GetActivityFn = (params: ActivitiesParams) => Promise<Activity[]>
 export const getFeedData: GetActivityFn = (params) => getActivitiesFrom('news_feed', params)
 export const getNotificationsData: GetActivityFn = (params) => getActivitiesFrom('notifications', params)
 
 export type GetCountFn = (account: string) => Promise<number>
-export const getFeedCount: GetCountFn = (account) => getCountFrom('news_feed', { account })
-export const getNotificationsCount: GetCountFn = (account) => getCountFrom('notifications', { account })
+export const getFeedCount: GetCountFn = (account) => getCountFrom('news_feed', account)
+export const getNotificationsCount: GetCountFn = (account) => getCountFrom('notifications', account)
