4aca3848cd24685358f0a93d31eedab6915f3d54
Vadyslav Samchuk <sam4uk.v1ad@gmail.com>
2020-10-21 20:38:52 +0300

    Change the name of variables on camel case

--- src/search/reindexer.ts
+++ src/search/reindexer.ts
@@ -6,18 +6,18 @@ import { indexPostContent, indexSpaceContent } from './event-handlers/utils';
 async function reindexContentFromIpfs() {
   const api = await Api.connect(process.env.SUBSTRATE_URL)
   const substrate = new SubsocialSubstrateApi(api)
-  
-  const last_space_id = await substrate.nextSpaceId()
-  const last_post_id = await substrate.nextPostId()
 
-  for (let i = 1; i < last_space_id.toNumber(); i++) {
+  const lastSpaceId = await substrate.nextSpaceId()
+  const lastPostId = await substrate.nextPostId()
+
+  for (let i = 1; i < lastSpaceId.toNumber(); i++) {
     const id: SpaceId = i as unknown as SpaceId
     const space = await substrate.findSpace({ id });
 
     indexSpaceContent(space)
   }
 
-  for (let i = 1; i < last_post_id.toNumber(); i++) {
+  for (let i = 1; i < lastPostId.toNumber(); i++) {
     const id: PostId = i as unknown as PostId
     const post = await substrate.findPost({ id });
 
