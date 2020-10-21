32df9a337c51a7b27231f278d1fc70df0be77250
Vadyslav Samchuk <sam4uk.v1ad@gmail.com>
2020-10-21 19:50:47 +0300

    Fix indentation
    - Remove unused comment

--- src/search/reindexer.ts
+++ src/search/reindexer.ts
@@ -3,30 +3,28 @@ import { Api } from '@subsocial/api/substrateConnect';
 import { PostId, SpaceId } from '@subsocial/types/substrate/interfaces';
 import { indexPostContent, indexSpaceContent } from './event-handlers/utils';
 
- async function reindexContentFromIpfs () {
-	const api = await Api.connect(process.env.SUBSTRATE_URL)
+async function reindexContentFromIpfs() {
+  const api = await Api.connect(process.env.SUBSTRATE_URL)
   const substrate = new SubsocialSubstrateApi(api)
   let last_space_id = await substrate.nextSpaceId()
 
-  // console.log("last space id:" ,last_space_id.toNumber())
-
   let last_post_id = await substrate.nextPostId()
 
-	for (let i = 1; i < last_space_id.toNumber(); i++) {
-		const space_id: SpaceId = i as unknown as SpaceId
-		const space = await substrate.findSpace({ id: space_id});
+  for (let i = 1; i < last_space_id.toNumber(); i++) {
+    const space_id: SpaceId = i as unknown as SpaceId
+    const space = await substrate.findSpace({ id: space_id });
 
     indexSpaceContent(space)
   }
-  
+
   for (let i = 1; i < last_post_id.toNumber(); i++) {
-		const post_id: PostId = i as unknown as PostId
-		const post = await substrate.findPost({ id: post_id});
+    const post_id: PostId = i as unknown as PostId
+    const post = await substrate.findPost({ id: post_id });
 
     indexPostContent(post)
   }
 
-	api.disconnect();
+  api.disconnect();
 }
 
 reindexContentFromIpfs()
