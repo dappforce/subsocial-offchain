a42ff35fc29bdca675a0aec3380699fcaa8be1e8
Vadyslav Samchuk <sam4uk.v1ad@gmail.com>
2020-10-21 20:11:33 +0300

    Refactor code style

--- src/search/reindexer.ts
+++ src/search/reindexer.ts
@@ -6,20 +6,20 @@ import { indexPostContent, indexSpaceContent } from './event-handlers/utils';
 async function reindexContentFromIpfs() {
   const api = await Api.connect(process.env.SUBSTRATE_URL)
   const substrate = new SubsocialSubstrateApi(api)
-  let last_space_id = await substrate.nextSpaceId()
-
-  let last_post_id = await substrate.nextPostId()
+  
+  const last_space_id = await substrate.nextSpaceId()
+  const last_post_id = await substrate.nextPostId()
 
   for (let i = 1; i < last_space_id.toNumber(); i++) {
-    const space_id: SpaceId = i as unknown as SpaceId
-    const space = await substrate.findSpace({ id: space_id });
+    const id: SpaceId = i as unknown as SpaceId
+    const space = await substrate.findSpace({ id });
 
     indexSpaceContent(space)
   }
 
   for (let i = 1; i < last_post_id.toNumber(); i++) {
-    const post_id: PostId = i as unknown as PostId
-    const post = await substrate.findPost({ id: post_id });
+    const id: PostId = i as unknown as PostId
+    const post = await substrate.findPost({ id });
 
     indexPostContent(post)
   }
