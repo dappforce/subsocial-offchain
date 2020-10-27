78958458acda58b5fca74c8d7657340a3c74b843
Vadyslav Samchuk <sam4uk.v1ad@gmail.com>
2020-10-27 19:56:16 +0200

    Use BN in reindexer

--- src/search/reindexer.ts
+++ src/search/reindexer.ts
@@ -4,29 +4,32 @@ import { PostId, SpaceId } from '@subsocial/types/substrate/interfaces';
 import { indexPostContent, indexSpaceContent } from './event-handlers/utils';
 import { elasticLog as log } from '../connections/loggers';
 
+const BN = require('bn.js');
+const one = new BN(1)
+
 async function reindexContentFromIpfs() {
   const api = await Api.connect(process.env.SUBSTRATE_URL)
   const substrate = new SubsocialSubstrateApi(api)
 
-  const lastSpaceId = await substrate.nextSpaceId()
-  const lastPostId = await substrate.nextPostId()
+  const lastSpaceId = (await substrate.nextSpaceId()).sub(one)
+  const lastPostId = (await substrate.nextPostId()).sub(one)
 
-  for (let i = 1; i < lastSpaceId.toNumber(); i++) {
+  for (let i = one; lastSpaceId.gte(i); i = i.add(one)) {
     const id: SpaceId = i as unknown as SpaceId
     const space = await substrate.findSpace({ id });
 
     indexSpaceContent(space)
 
-    log.info(`Index post content from IPFS (${lastSpaceId.toNumber() - 1}): ${id}`)
+    log.info(`Index space # ${id.toString()} out of ${lastSpaceId.toString()}`)
   }
 
-  for (let i = 1; i < lastPostId.toNumber(); i++) {
+  for (let i = one; lastPostId.gte(i); i = i.add(one)) {
     const id: PostId = i as unknown as PostId
     const post = await substrate.findPost({ id });
 
     indexPostContent(post)
 
-    log.info(`Index post content from IPFS (${lastPostId.toNumber() - 1}): ${id}`)
+    log.info(`Index post # ${id.toString()} out of ${lastPostId.toString()}`)
   }
 
   api.disconnect();
