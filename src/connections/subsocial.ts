b10e4f82294d4cea13126dbe3a861ecd433f1bd3
Vadyslav Samchuk <sam4uk.v1ad@gmail.com>
2020-11-13 16:25:41 +0200

    WIP: add mock Substrate

--- src/connections/subsocial.ts
+++ src/connections/subsocial.ts
@@ -3,17 +3,36 @@ import { SubsocialApi } from '@subsocial/api/subsocial'
 import { SubsocialSubstrateApi } from '@subsocial/api/substrate'
 import { Api } from '@subsocial/api/substrateConnect'
 import { ipfsConfig } from './ipfs'
+import { readFileSync } from 'fs';
+import { Post, SocialAccount, Space } from '@subsocial/types/substrate/interfaces';
 
 export let subsocial: SubsocialApi
 export let substrate: SubsocialSubstrateApi
 export let ipfs: SubsocialIpfsApi
 
+const createMockSubstrate = (): SubsocialSubstrateApi => {
+  const postById: Record<string, Post> = JSON.parse(readFileSync('../test/input_data/posts.json', 'utf-8'))
+  const spaceById: Record<string, Space> = JSON.parse(readFileSync('../test/input_data/spaces.json.json', 'utf-8'))
+  const profileByAccount: Record<string, SocialAccount> = JSON.parse(readFileSync('../test/input_data/profiles.json.json', 'utf-8'))
+
+  return {
+    findPost: async ({ id }) => postById[id.toString()],
+    findSpace: async ({ id }) => spaceById[id.toString()],
+    findSocialAccount: async (id) => profileByAccount[id.toString()]
+  } as SubsocialSubstrateApi
+}
 /**
  * Create a new or return existing connection to Subsocial API
  * (includes Substrate and IPFS connections).
  */
 export const resolveSubsocialApi = async () => {
   // Connect to Subsocial's Substrate node:
+  if (process.env.TEST_MODE?.toLocaleLowerCase() === 'true') {
+    return {
+      substrate: createMockSubstrate()
+    }
+  }
+
   if (!subsocial) {
     const api = await Api.connect(process.env.SUBSTRATE_URL)
     subsocial = new SubsocialApi({
--- src/test/input_data/getStructsFromSubstrate.js
+++ src/test/input_data/getStructsFromSubstrate.js
@@ -1,25 +1,32 @@
-const { resolveSubsocialApi } = require('./connections/subsocial')
+const { resolveSubsocialApi } = require('../../../build/src/connections/subsocial')
 const { writeFileSync } = require('fs')
+const { GenericAccountId } = require('@polkadot/types/generic')
 
 const getData = async () => {
   const { substrate } = await resolveSubsocialApi()
 
   const spacesEntry = await (await substrate.api).query.spaces.spaceById.entries()
 
-  const spaces = spacesEntry.map(x => JSON.parse(x[1])).filter(x => x.id > 1000)
-  writeFileSync('./spaces.json', JSON.stringify(spaces))
-
+  const spacesStore = {}
+  const postStore = {}
+  const profileStore = {}
+  const spaces = spacesEntry.map(x => JSON.parse(x[1])).filter(x => x.id > 1000).forEach(x => { spacesStore[x.id] = x })
+  writeFileSync('./spaces.json', JSON.stringify(spacesStore))
 
   const postEntry = await (await substrate.api).query.posts.postById.entries()
 
-  const posts = postEntry.map(x => JSON.parse(x[1]))
-  writeFileSync('./posts.json', JSON.stringify(posts))
+  const posts = postEntry.map(x => JSON.parse(x[1])).forEach(x => { postStore[x.id] = x })
+  writeFileSync('./posts.json', JSON.stringify(postStore))
 
 
-  const profilesEntry = await (await substrate.api).query.spaces.spaceById.entries()
-
-  const profiles = profilesEntry.map(x => JSON.parse(x[1]))
-  writeFileSync('./profiles.json', JSON.stringify(profiles))
+  const profilesEntry = await (await substrate.api).query.profiles.socialAccountById.entries()
+    
+  const profiles = profilesEntry.forEach(([ key, socialAccount ]) => {
+    const addressEncoded = '0x' + key.toHex().substr(-64)
+    const account = new GenericAccountId(key.registry, addressEncoded)
+    profileStore[account] = JSON.parse(socialAccount)
+  })
+  writeFileSync('./profiles.json', JSON.stringify(profileStore))
 
   process.exit(0)
 }
