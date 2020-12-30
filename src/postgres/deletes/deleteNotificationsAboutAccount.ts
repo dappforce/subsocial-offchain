764ce198e574f1bbbca3a656408257a9694c27da
Alex Siman <aleksandr.siman@gmail.com>
2020-12-30 21:45:58 +0200

    Add missing "FROM df.activities" to SQL query of deleteNotificationsAboutAccount

--- src/postgres/deletes/deleteNotificationsAboutAccount.ts
+++ src/postgres/deletes/deleteNotificationsAboutAccount.ts
@@ -5,6 +5,7 @@ const query = `
   DELETE FROM df.notifications
   WHERE account = $1 AND (block_number, event_index) IN (
     SELECT block_number, event_index
+    FROM df.activities
     LEFT JOIN df.accountblock_number_followers
     ON df.activities.account = df.account_followers.following_account
     WHERE account = $2
