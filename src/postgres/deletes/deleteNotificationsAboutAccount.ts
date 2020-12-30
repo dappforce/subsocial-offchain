db993a32e484bd201cfbfa2303b4ea66bc601a74
Alex Siman <aleksandr.siman@gmail.com>
2020-12-30 22:08:16 +0200

    Fix SQL query of deleteNotificationsAboutAccount

--- src/postgres/deletes/deleteNotificationsAboutAccount.ts
+++ src/postgres/deletes/deleteNotificationsAboutAccount.ts
@@ -6,7 +6,7 @@ const query = `
   WHERE account = $1 AND (block_number, event_index) IN (
     SELECT block_number, event_index
     FROM df.activities
-    LEFT JOIN df.accountblock_number_followers
+    LEFT JOIN df.account_followers
     ON df.activities.account = df.account_followers.following_account
     WHERE account = $2
   )
