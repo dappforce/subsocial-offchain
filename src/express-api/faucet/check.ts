1b8563190bf2ea342d3edde98642a49c0fbc9b51
olehmell <oleh.melnechyk@gmail.com>
2021-01-20 19:10:43 +0200

    Add check drop cases function

--- src/express-api/faucet/check.ts
+++ src/express-api/faucet/check.ts
@@ -1,6 +1,33 @@
+import { resolveSubsocialApi } from '../../connections';
+import { checkDropByAccountAndEmail } from '../../postgres/selects/checkDropByAccountAndEmail';
+import { OkOrError } from '../utils';
 import { FaucetFormData } from "./types";
 
-export async function checkEmail(_formData: FaucetFormData): Promise<boolean> {
-  // TODO: checkEmail in database
-  return true;
+export async function checkWasTokenDrop({ account, email }: Omit<FaucetFormData, 'token'>): Promise<OkOrError> {
+  const { account: foundAccount, email: foundEmail } = await checkDropByAccountAndEmail(account, email)
+
+  const errors: Record<string, string> = {}
+  let ok = true
+
+  if (foundAccount === account) { 
+    ok = false
+    errors.account = 'The account was drop token yet'
+  }
+
+  if (foundEmail === email) { 
+    ok = false
+    errors.account = 'The email was drop token yet'
+  }
+
+  const { substrate: { api } } = await resolveSubsocialApi()
+  const readyApi = await api
+
+  const { freeBalance } = await readyApi.derive.balances.all(account)
+  
+  if (!freeBalance.eqn(0)) { 
+    ok = false
+    errors.account = 'The account haven\'t zero balance'
+  }
+
+  return { ok, errors };
 }
--- src/express-api/faucet/types.ts
+++ src/express-api/faucet/types.ts
@@ -1,5 +1,20 @@
 export type FaucetFormData = {
-  address: string,
+  account: string,
   email: string,
   token: string
+}
+
+export type TokenDropsColumns = {
+  faucet: string,
+  account: string,
+  amount: number,
+  captcha_solved: boolean,
+  email?: string,
+  telegram_id?: string,
+  discord_id?: string
+}
+
+export type BaseConfirmData = {
+  account: string,
+  confirmationCode: string
 }
