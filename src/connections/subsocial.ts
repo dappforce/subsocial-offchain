ba876ef083264e6de620cb4ea8a1e3cae53ebb5b
olehmell <oleh.melnechyk@gmail.com>
2021-01-21 18:48:26 +0200

    Improve resolveSubsocialApi

--- src/connections/subsocial.ts
+++ src/connections/subsocial.ts
@@ -2,29 +2,37 @@ import { SubsocialIpfsApi } from '@subsocial/api/ipfs'
 import { SubsocialApi } from '@subsocial/api/subsocial'
 import { SubsocialSubstrateApi } from '@subsocial/api/substrate'
 import { Api } from '@subsocial/api/substrateConnect'
+import { registry } from '@subsocial/types/substrate/registry'
 import { ipfsConfig } from './ipfs'
+import { ApiPromise } from '@polkadot/api'
 
 export let subsocial: SubsocialApi
 export let substrate: SubsocialSubstrateApi
 export let ipfs: SubsocialIpfsApi
-
+export let api: ApiPromise
 /**
  * Create a new or return existing connection to Subsocial API
  * (includes Substrate and IPFS connections).
  */
-export const resolveSubsocialApi = async () => {
+
+type Api = SubsocialApi & {
+  api: ApiPromise
+}
+
+export const resolveSubsocialApi = async (): Promise<Api> => {
   // Connect to Subsocial's Substrate node:
 
   if (!subsocial) {
-    const api = await Api.connect(process.env.SUBSTRATE_URL)
+    api = await Api.connect(process.env.SUBSTRATE_URL)
+    const properties = await api.rpc.system.properties()
+
+    registry.setChainProperties(properties)
     subsocial = new SubsocialApi({
       substrateApi: api,
       ...ipfsConfig
     })
 
-    substrate = subsocial.substrate
-    ipfs = subsocial.ipfs
   }
 
-  return subsocial
+  return { api, ...subsocial } as unknown as Api
 }
