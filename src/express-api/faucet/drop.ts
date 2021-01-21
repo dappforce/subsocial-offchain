bc147b167e857ba89f17518f7995802ead733443
olehmell <oleh.melnechyk@gmail.com>
2021-01-21 18:55:12 +0200

    Implemented new logic for drop func

--- src/express-api/faucet/drop.ts
+++ src/express-api/faucet/drop.ts
@@ -1,30 +1,41 @@
+import { GenericAccountId } from '@polkadot/types/generic';
+import { registry } from '@subsocial/types/substrate/registry';
 import { faucetAmount } from '../../env';
 import { insertTokenDrop } from '../../postgres/inserts/insertTokenDrop';
 import { getConfirmationData } from '../../postgres/selects/getConfirmationCode';
 import { setConfirmationDate } from '../../postgres/updates/setConfirmationDate';
 import { OkOrError } from '../utils';
 import { checkWasTokenDrop } from './check';
-import { faucetPublicKey, transferToken } from './transfer';
+import { getFaucetPublicKey, transferToken } from './transfer';
 import { BaseConfirmData } from "./types";
 
-export const tokenDrop = async (confirmData: BaseConfirmData): Promise<OkOrError> => {
-  const { account } = confirmData
-
+export const tokenDrop = async ({ account: clientSideAccount, confirmationCode }: BaseConfirmData): Promise<OkOrError<number>> => {
+  const account = new GenericAccountId(registry, clientSideAccount).toString()
   try {
     const { email } = await getConfirmationData(account)
-    const isConfirmed = await setConfirmationDate(confirmData)
+    const isConfirmed = await setConfirmationDate({ account, confirmationCode })
     const { ok: noTokenDrop, errors } = await checkWasTokenDrop({ account, email })
 
-    if (isConfirmed && noTokenDrop) {
-      await insertTokenDrop({ faucet: faucetPublicKey, account, amount: faucetAmount, email, captcha_solved: true })
-      await transferToken()
-      return { ok: true }
+    console.log(isConfirmed, noTokenDrop)
+    if (/* isConfirmed && */noTokenDrop) {
+      await transferToken(account,
+        (block_number, event_index) => insertTokenDrop({
+          block_number,
+          event_index,
+          faucet: getFaucetPublicKey(),
+          account,
+          amount: faucetAmount,
+          email,
+          captcha_solved: true
+        }))
+
+      return { ok: true, data: faucetAmount }
     } else {
       throw errors
     }
 
   } catch (err) {
-    return { ok: false, errors: err }
+    return { ok: false, errors: err?.stack || err }
   }
 
 }
