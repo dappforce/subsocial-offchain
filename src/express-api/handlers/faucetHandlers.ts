c70825ad94da3b59e6b8165f8384c3817e23d681
olehmell <oleh.melnechyk@gmail.com>
2021-01-21 18:56:17 +0200

    Add drop faucet handler

--- src/express-api/handlers/faucetHandlers.ts
+++ src/express-api/handlers/faucetHandlers.ts
@@ -5,24 +5,37 @@ import { checkWasTokenDrop } from '../faucet/check';
 import { BaseConfirmData, FaucetFormData } from '../faucet/types';
 import { HandlerFn } from '../utils';
 import { tokenDrop } from '../faucet/drop';
+import { GenericAccountId } from '@polkadot/types';
+import { registry } from '@subsocial/types/substrate/registry';
+import isEmpty from 'lodash.isempty'
+
+const getSubsocialAccountId = (account: string) => new GenericAccountId(registry, account).toString()
 
 export const confirmEmailHandler: HandlerFn = async (req, res) => {
   const formData: FaucetFormData = req.body;
 
-  const human = await validateHuman(formData.token);
+  if (isEmpty(formData)) {
+    res.status(400).send({ errors: { account: 'Invalid data' } })
+  }
+  
+  const account = getSubsocialAccountId(formData.account)
+
+  const data = { ...formData, account }
+
+  const human = await validateHuman(data.token);
   if (!human) {
     res.status(400);
-    res.json({ error: 'Please, you\'re not fooling us, bot.' });
+    res.json({ errors: { account: 'Please, you\'re not fooling us, bot.' } });
   }
 
-  const { ok, errors } = await checkWasTokenDrop(formData)
+  const { ok, errors } = await checkWasTokenDrop(data)
 
   console.log('errors', errors, ok)
 
   if (ok) {
-    const confirmationCode = await sendFaucetConfirmationLetter(formData.email)
+    const confirmationCode = await sendFaucetConfirmationLetter(data)
     console.log('confirmationCode', confirmationCode)
-    confirmationCode && await addEmailWithConfirmCode({ ...formData, confirmationCode })
+    confirmationCode && await addEmailWithConfirmCode({ ...data, confirmationCode })
     res.status(200);
     res.json({ ok });
   } else {
@@ -33,8 +46,15 @@ export const confirmEmailHandler: HandlerFn = async (req, res) => {
 
 export const tokenDropHandler: HandlerFn = async (req, res) => {
   const data = req.body as BaseConfirmData
-  const { ok, errors } = await tokenDrop(data)
 
+  if (isEmpty(data)) {
+    res.status(400).send({ errors: { account: 'Invalid data' } })
+  }
+
+  const account = getSubsocialAccountId(data.account)
+
+  const { ok, errors } = await tokenDrop({ ...data, account })
+  console.log('tokenDrop', ok, errors)
   if (ok) {
     res.status(200).send({ ok })
   } else {
