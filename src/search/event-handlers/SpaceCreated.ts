e6df1337efd3d57f9337131e97b181a013fb2c93
olehmell <oleh.melnechyk@gmail.com>
2021-01-26 20:07:53 +0200

    Improve imports subsocial api

--- src/search/event-handlers/SpaceCreated.ts
+++ src/search/event-handlers/SpaceCreated.ts
@@ -1,11 +1,13 @@
 import { SpaceId } from '@subsocial/types/substrate/interfaces/subsocial';
-import { substrate } from '../../connections/subsocial';
+import { resolveSubsocialApi } from '../../connections';
 import { EventHandlerFn } from '../../substrate/types';
 import { indexSpaceContent } from '../indexer';
 
 export const onSpaceCreated: EventHandlerFn = async (eventAction) => {
   const { data } = eventAction;
   const spaceId = data[1] as SpaceId;
+  const { substrate } = await resolveSubsocialApi()
+
   const space = await substrate.findSpace({ id: spaceId });
   if (!space) return;
 
--- src/search/event-handlers/SpaceUpdated.ts
+++ src/search/event-handlers/SpaceUpdated.ts
@@ -1,11 +1,14 @@
 import { SpaceId } from '@subsocial/types/substrate/interfaces/subsocial';
-import { substrate } from '../../connections/subsocial';
+import { resolveSubsocialApi } from '../../connections/subsocial';
 import { EventHandlerFn } from '../../substrate/types';
 import { indexSpaceContent } from '../indexer';
 
 export const onSpaceUpdated: EventHandlerFn = async (eventAction) => {
   const { data } = eventAction;
   const spaceId = data[1] as SpaceId;
+
+  const { substrate } = await resolveSubsocialApi()
+
   const space = await substrate.findSpace({ id: spaceId });
   if (!space) return;
 
