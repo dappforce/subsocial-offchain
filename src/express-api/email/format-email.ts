dd8590d77d6cf09e8a9767e51a62c1bbbefefeff
olehmell <oleh.melnechyk@gmail.com>
2021-02-10 16:57:10 +0200

    Split function

--- src/express-api/email/format-email.ts
+++ src/express-api/email/format-email.ts
@@ -1,10 +1,10 @@
+// FIXME: export these to JS libs
+export const parseEmail = (email: string) => email?.trim().toLowerCase().split('@')
+
 export const formatEmail = (email?: string) => {
 	if (!email) return ''
 
-	const [ username, domain ] = email
-		.trim()
-		.toLowerCase()
-		.split('@')
+	const [ username, domain ] = parseEmail(email)
 
 	return `${username.replace(/[\W_]/g, '')}@${domain}`
 }
