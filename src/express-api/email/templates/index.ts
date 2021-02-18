971aa41e7b2a969b315f23236b2f5a31ffcd3161
olehmell <oleh.melnechyk@gmail.com>
2021-02-18 17:43:25 +0200

    Refactor templates

--- src/express-api/email/templates/index.ts
+++ src/express-api/email/templates/index.ts
@@ -1,17 +1,31 @@
 import { readFileSync } from "fs";
 import { compile } from "handlebars";
+// import { FeedTemplateProp } from "../types";
 
-export type TemplateType = 'notifications' | 'feeds' | 'confirmation'
+export type TemplateType = 'notifications' | 'feed' | 'confirmation'
 
-const getTemplate = (type) => {
-  const source = readFileSync(`${__dirname}/${type}/html.hbs`, 'utf8');
+const getTemplate = (path) => {
+  const source = readFileSync(`${__dirname}/${path}.hbs`, 'utf8');
 	return compile(source)
 }
 
+const getGeneralTemplate = (type: 'confirmation' | 'activity') => getTemplate(`${type}/html`)
+
+// const activityTemplate = getGeneralTemplate('activity')
+
+const getActivity = (type: 'feed' | 'notifications') => getTemplate(`activity/${type}`)
+// {
+//   const template = getTemplate(`activity/${type}`)
+
+//   return (data: FeedTemplateProp) => activityTemplate({
+//     data: template({ data })
+//   })
+// }
+
 export const templates: Record<TemplateType, HandlebarsTemplateDelegate<any>> = { 
-  notifications: getTemplate('notifications'),
-  feeds: getTemplate('feeds'),
-  confirmation: getTemplate('confirmation')
+  notifications: getActivity('notifications'),
+  feed: getActivity('feed'),
+  confirmation: getGeneralTemplate('confirmation')
 }
 
 export default templates
