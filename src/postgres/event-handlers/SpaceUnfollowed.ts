ba8dbbed45970b57dca9a62ddb537c0d6949aa04
Vladyslav Samchuk <sam4uk.v1ad@gmail.com>
2021-03-04 18:25:16 +0200

    Fix news feed when account unfollowed

--- src/postgres/event-handlers/SpaceUnfollowed.ts
+++ src/postgres/event-handlers/SpaceUnfollowed.ts
@@ -1,4 +1,5 @@
 import { EventHandlerFn } from '../../substrate/types';
+import { deleteNewsFeed } from '../deletes/deleteNewsFeed';
 import { deleteNotificationsAboutSpace } from '../deletes/deleteNotificationsAboutSpace';
 import { deleteSpaceFollower } from '../deletes/deleteSpaceFollower';
 
@@ -8,4 +9,5 @@ export const onSpaceUnfollowed: EventHandlerFn = async (eventAction) => {
   const following = data[1].toString();
   await deleteNotificationsAboutSpace(follower, following)
   await deleteSpaceFollower(data);
+  await deleteNewsFeed(data)
 }
