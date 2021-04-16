a092786b36fed5f0b1adbc57beb4e8112660d7c6
olehmell <oleh.melnechyk@gmail.com>
2021-04-16 12:17:59 +0300

    Small fix migration

--- src/postgres/migrations/9-alter-token-drop.sql
+++ src/postgres/migrations/9-alter-token-drop.sql
@@ -1,3 +1,2 @@
-ALTER TABLE df.token_drops ADD column date TIMESTAMP NOT NULL DEFAULT NOW();
 ALTER TABLE df.token_drops DROP CONSTRAINT token_drops_pkey;
 ALTER TABLE df.token_drops ADD PRIMARY KEY (block_number, event_index);
