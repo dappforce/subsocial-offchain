8988e1bc66b824121b96a3b80480eddeaa434ad0
Vladyslav Proshchavaiev <f3joule@gmail.com>
2021-05-15 10:30:45 +0300

    Make compose.sh file simpler

--- docker/compose.sh
+++ docker/compose.sh
@@ -4,73 +4,70 @@ set -e
 pushd . > /dev/null
 
 # The following line ensure we run from the script folder
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-eval cd $DIR
+CDIR=`dirname "$0"`
+cd "$CDIR"
 
-UTILS_ONLY=false
+# TODO: add selective start of components
+
+# POSTGRES_SERVICE="postgres"
+# ELASTIC_SERVICE="elasticsearch"
+# IPFS_SERVICES='ipfs-cluster ipfs-peer'
+# COMPOSE_FILES="$POSTGRES_SERVICE $ELASTIC_SERVICE $IPFS_SERVICES"
+
+export ELASTIC_CONTAINER="subsocial-elasticsearch"
+export POSTGRES_CONTAINER="subsocial-postgres"
+export IPFS_NODE_CONTAINER="subsocial-ipfs-node"
+export IPFS_CLUSTER_CONTAINER="subsocial-ipfs-cluster"
+
+ES_NODE_URL='http://127.0.0.1:9200'
+IPFS_NODE_URL='http://127.0.0.1:8080'
 
 # Generated new IPFS Cluster secret in case the 'ipfs-data' directory was deleted
 export CLUSTER_SECRET=$(od  -vN 32 -An -tx1 /dev/urandom | tr -d ' \n')
 
 case $1 in
-  --down)
-    time docker-compose kill
-    if [[ $2 == 'clean' ]]; then
+  --stop)
+    docker-compose kill
+    if [[ $2 == '--clean' ]]; then
       echo "Cleaning volumes..."
       docker-compose down -v
+
+      while true; do
+        read -p "Do you want to remove ipfs folder as well? [Y/n]:" yn
+        case $yn in
+          [Yy]*) sudo rm -rf ipfs-data; break;;
+          [Nn]*) break;;
+          * ) echo "Please answer yes or no.";;
+        esac
+      done
     fi
     exit 0
-    ;;
-  --utils)
-    UTILS_ONLY=true
     ;;
   -?*)
     printf "Invalid argument provided.\n\nExamples:\n"
     printf "Start all:\n./compose.sh\n\n"
-    printf "Start services without offchain itself:\n./compose.sh --utils\n\n"
     printf "Stop all:\n./compose.sh --down\n\n"
     printf "Clean all:\n./compose.sh --down clean\n"
 
     exit 1
     ;;
   --)
-    UTILS_ONLY=false
     ;;
 esac
 
-UTILS=" postgres elasticsearch ipfs-cluster"
+printf "Starting offchain utils in background, hang on!\n\n"
 
-ES_NODE_URL='http://127.0.0.1:9200'
-IPFS_NODE_URL='http://127.0.0.1:8080'
-
-printf "Starting offchain in background, hang on!\n\n"
-
-if $UTILS_ONLY; then
-  docker-compose up -d $UTILS
-else
-  docker-compose up -d
-  eval docker stop subsocial-offchain &> /dev/null
-
-  printf "\nStarting Elasticsearch...\n"
-  until curl -s $ES_NODE_URL > /dev/null; do
-    sleep 2
-  done
-  docker-compose up -d offchain
-fi
-
-printf "\nWaiting until IPFS is ready...\n"
-until curl -s "${IPFS_NODE_URL}/api/v0/version" > /dev/null
-do
-  sleep 1
-done
-
-docker exec subsocial-ipfs-node \
-  ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["*"]'
-docker exec subsocial-ipfs-node \
-  ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["GET", "PUT", "POST"]'
-docker exec subsocial-ipfs-node ipfs bootstrap rm --all &> /dev/null
+docker-compose up -d $UTILS
 
 printf "Configuring IPFS node...\n\n"
+until curl -s "$IPFS_NODE_URL/api/v0/version" > /dev/null; do
+  sleep 2
+done
+
+docker exec subsocial-ipfs-node ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["*"]'
+docker exec subsocial-ipfs-node ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["GET", "PUT", "POST"]'
+docker exec subsocial-ipfs-node ipfs bootstrap rm --all &> /dev/null
+
 docker restart subsocial-ipfs-node > /dev/null
 
 echo "Containers are ready."
