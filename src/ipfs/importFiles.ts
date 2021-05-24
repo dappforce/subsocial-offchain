def45c7a41376826ce4e6a548f017b50d63d3108
Vladyslav Samchuk <sam4uk.v1ad@gmail.com>
2021-05-24 16:11:34 +0300

    Separate import content and import files

--- src/ipfs/importFiles.ts
+++ src/ipfs/importFiles.ts
@@ -1,33 +1,28 @@
 import { readdirSync, readFileSync } from 'fs'
-import { backupPath, ipfsClusterUrl } from '../env'
+import { backupPath } from '../env'
 import { entities } from './utils'
-import { ipfsLog as log } from '../connections/loggers'
-import request from 'request-promise'
+import { ipfsCluster } from '../connections/ipfs';
 
-const uploadFiles = async () => {
+const importFiles = async () => {
   for (const entity of entities) {
-    const files = readdirSync(`${backupPath}/${entity}/files`)
+    let files = readdirSync(`${backupPath}/${entity}/files`)
 
-    const filesPromise = files.map(async file => {
-      const uploadImg = readFileSync(`${backupPath}/${entity}/files/${file}`)
+    for (const file of files) {
+      let uploadImg = readFileSync(`${backupPath}/${entity}/files/${file}`)
 
       if (Buffer.isBuffer(uploadImg)) {
-        const data = {
-          value: uploadImg,
-          options: { filename: file, contentType: '' }
-        }
+        const fileData = {
+          buffer: uploadImg,
+          originalname: file,
+          mimetype: ''
+        } as any
 
-        try {
-          await request(`${ipfsClusterUrl}/add`, { method: 'POST', formData: { '': data } })
-          log.info('Content added and pinned under CID:', file)
-        } catch (err) {
-          log.error(file, err)
-        }
+        await ipfsCluster.addFile(fileData)
       }
-    })
-
-    Promise.all(filesPromise)
+      uploadImg = null
+    }
+    files = null
   }
 }
 
-uploadFiles()
+importFiles()
--- src/ipfs/importIpfsData.ts
+++ src/ipfs/importIpfsData.ts
@@ -2,21 +2,21 @@ import { readFileSync } from 'fs'
 import { ipfsCluster } from '../connections/ipfs'
 import { backupPath } from '../env'
 import { ipfsLog as log } from '../connections/loggers'
-import { CommonContent } from '@subsocial/types/offchain';
 import { entities } from './utils';
 
-const importIpfsData = async () => {
+const importContents = async () => {
   for (const entity of entities) {
-    const content: CommonContent = JSON.parse(readFileSync(`${backupPath}/${entity}/content.json`, 'utf8'))
+    let contents = Object.entries(JSON.parse(readFileSync(`${backupPath}/${entity}/content.json`, 'utf8')))
     log.info(`Load ${entity} content`)
-    for (const [key, value] of Object.entries(content)) {
+    for (const [key, value] of contents) {
       try {
         await ipfsCluster.addContent(JSON.stringify(value))
       } catch (err) {
         log.error(key, err)
       }
     }
+    contents = null
   }
 }
 
-importIpfsData()
+importContents()
