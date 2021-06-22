c21a065b2fbdf32ef9860ed2cac544873326cfd8
olehmell <oleh.melnechyk@gmail.com>
2021-06-22 09:25:43 +0300

    Add resolve image for feed

--- src/express-api/email/feed.ts
+++ src/express-api/email/feed.ts
@@ -1,44 +1,53 @@
 import { Activity } from '../telegramWS';
 import { resolveSubsocialApi } from '../../connections/subsocial';
 import { PostId } from '@subsocial/types/substrate/interfaces';
-import { createHrefForPost, getAccountContent, getFormatDate, toShortAddress } from './utils';
+import { createHrefForPost, getAccountContent, getFormatDate, resolveIpfsUrl, toShortAddress } from './utils';
 import { FeedTemplateProp } from './types';
 import { summarizeMd } from '@subsocial/utils/summarize'
 import { PostWithAllDetails } from '@subsocial/types';
 
 const createPostData = async ({ post, space }: PostWithAllDetails) => {
-	const { id, owner, space_id, created: { time } } = post.struct
+  const { id, owner, space_id, created: { time } } = post.struct
 
-	const { title: postTitle, body, image } = post.content
+  const { title: postTitle, body, image } = post.content
 
-	const { summary: postSummary } = summarizeMd(body)
+  const { summary: postSummary } = summarizeMd(body)
 
-	const { name: spaceName } = space.content
+  const { name: spaceName } = space.content
 
-	const ownerAddress = owner.toString()
+  const ownerAddress = owner.toString()
 
-	const { name: ownerName = toShortAddress(ownerAddress), avatar } = await getAccountContent(ownerAddress)
+  const { name: ownerName = toShortAddress(ownerAddress), avatar } = await getAccountContent(ownerAddress)
 
-	const postLink = createHrefForPost(space_id.toString(), id.toString())
-	
-	return { ownerName, avatar, spaceName, postTitle, postLink, postSummary, date: getFormatDate(time.toString()), image }
+  const postLink = createHrefForPost(space_id.toString(), id.toString())
+
+  return {
+    ownerName,
+    avatar,
+    spaceName,
+    postTitle,
+    postLink,
+    postSummary,
+    date: getFormatDate(time.toString()),
+    image: resolveIpfsUrl(image)
+  }
 }
 
 export const createFeedEmailMessage = async (activity: Activity): Promise<FeedTemplateProp> => {
-	const { post_id } = activity
-	const subsocial = await resolveSubsocialApi()
-	const post = await subsocial.findPostWithAllDetails(post_id as unknown as PostId)
-	
-	const { extension } = post.post.struct
+  const { post_id } = activity
+  const subsocial = await resolveSubsocialApi()
+  const post = await subsocial.findPostWithAllDetails(post_id as unknown as PostId)
 
-	const postData = await createPostData(post)
-	if (extension.isSharedPost) {
-		const extPost = await subsocial.findPostWithAllDetails(extension.asSharedPost)
+  const { extension } = post.post.struct
 
-		const ext = await createPostData(extPost)
+  const postData = await createPostData(post)
+  if (extension.isSharedPost) {
+    const extPost = await subsocial.findPostWithAllDetails(extension.asSharedPost)
 
-		return { ...postData, ext }
-	}
+    const ext = await createPostData(extPost)
 
-	return postData
+    return { ...postData, ext }
+  }
+
+  return postData
 }
