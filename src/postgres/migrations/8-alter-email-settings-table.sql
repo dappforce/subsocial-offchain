7fef61cebbd6ab3381097a80ad28e3618aa31c72
Vladyslav Samchuk <sam4uk.v1ad@gmail.com>
2021-06-22 18:38:07 +0300

    Filling a formatted_email field

--- src/postgres/migrations/8-alter-email-settings-table.sql
+++ src/postgres/migrations/8-alter-email-settings-table.sql
@@ -1,2 +1,12 @@
 alter table df.email_settings rename column email to original_email;
-alter table df.email_settings add column formatted_email text null
+alter table df.email_settings add column formatted_email text null;
+
+UPDATE df.email_settings AS settings
+SET formatted_email = (
+	SELECT LOWER(CONCAT(REGEXP_REPLACE(SPLIT_PART(original_email, '@', 1), '[^a-zA-Z1-9]+', '', 'g'), '@', SPLIT_PART(original_email, '@', 2)))
+	FROM df.email_settings AS selected
+	WHERE selected.account = settings.account
+)
+WHERE settings.formatted_email IS NULL;
+
+ALTER TABLE df.email_settings ALTER COLUMN formatted_email SET NOT NULL
