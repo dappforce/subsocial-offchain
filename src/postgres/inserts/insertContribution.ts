093277037d593821992ee0c43aea7164922f2257
olehmell <oleh.melnechyk@gmail.com>
2021-11-04 13:29:56 +0200

    Check contribution to blockchain

--- src/postgres/inserts/insertContribution.ts
+++ src/postgres/inserts/insertContribution.ts
@@ -1,29 +1,59 @@
 import { newPgError, runQuery } from '../utils'
-import BN from 'bn.js'
 import { log } from '../postges-logger'
+import { getApi } from '@subsocial/api'
+import { equalAddresses } from '../../utils'
 
 export type Contribution = {
   contributor: string
   refCode: string
-  amount: BN
+  blockHash: string
+  eventIndex: string
 }
 
 const query = `
-  INSERT INTO df.referral_contributions(contributor, ref_code, amount, date)
-  VALUES(:contributor, :refCode, :amount, :date)
+  INSERT INTO df.referral_contributions(block_number, event_index, contributor, ref_code, amount, date)
+  VALUES(:blockNumber, :eventIndex, :contributor, :refCode, :amount, :date)
   ON CONFLICT (contributor, ref_code) DO UPDATE
   SET amount = df.referral_contributions.amount + :amount
   RETURNING *`
 
-export async function insertContribution(data: Contribution) {
-  const { contributor, refCode, amount } = data
+const SUBSOCIAL_PARA_ID = 2000
 
+const getKusamaApi = () => getApi('wss://staging.subsocial.network/kusama')
+
+export async function insertContribution(data: Contribution) {
+  const { contributor, refCode, blockHash, eventIndex } = data
+  console.log('{ contributor, refCode, blockHash, eventIndex }', { contributor, refCode, blockHash, eventIndex })
+  const kusamaApi = await getKusamaApi()
+
+  const events = await kusamaApi.query.system.events.at(blockHash)
+
+  const event = events[eventIndex]
+
+  if (!event) return
+
+  const [ account, paraId, amount ] = event.data
+  
   if(contributor === refCode) {
     log.warn('Contributor and referral code are equal', refCode)
     return
   }
 
+  if (paraId.toNumber() !== SUBSOCIAL_PARA_ID) {
+    log.warn('The no Subsocial contribute', refCode)
+    return
+  }
+
+  if (equalAddresses(account, contributor)) {
+    log.warn('Incorrect contributor', contributor)
+    return
+  }
+
+  const header = await kusamaApi.rpc.chain.getHeader(blockHash)
+
   const params = {
+    blockNumber: header.number.toBigInt(),
+    eventIndex,
     contributor,
     refCode,
     amount,
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import { asAccountId } from '@subsocial/api';
+import { AnyAccountId } from '@subsocial/types';
 import { readFile, writeFile, mkdir } from 'fs';
 import { join } from 'path';
 import { promisify } from 'util'
@@ -7,3 +9,15 @@ export const stateDirPath = join(__dirname, '../state')
 export const asyncReadFile = promisify(readFile)
 export const asyncWriteFile = promisify(writeFile)
 export const asyncMkdir = promisify(mkdir)
+
+type MaybeAccAddr = undefined | AnyAccountId
+
+export function equalAddresses (addr1: MaybeAccAddr, addr2: MaybeAccAddr): boolean {
+  if (addr1 === addr2) {
+    return true
+  } else if (!addr1 || !addr2) {
+    return false
+  } else {
+    return asAccountId(addr1)?.eq(asAccountId(addr2)) || false
+  }
+}
