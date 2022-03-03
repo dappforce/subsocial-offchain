3dec1926fc28fa192a280096f4304c9e720756e3
olehmell <oleh.melnechyk@gmail.com>
2022-03-03 19:34:32 +0200

    Update to new version libs

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,11 @@ import { resolveSubsocialApi } from "./connections"
 import { startHttpServer } from "./express-api/server"
 import { startSubstrateSubscriberWithWebSockets } from "./substrate/subscribe"
 
+import '@subsocial/definitions/interfaces/augment-api-query'
+import '@subsocial/definitions/interfaces/augment-api-events'
+import '@subsocial/definitions/interfaces/augment-api-tx'
+import '@subsocial/definitions/interfaces/augment-api-consts'
+
 const startOffchainServices = async () => {
   const { substrate } = await resolveSubsocialApi()
   await startHttpServer()
