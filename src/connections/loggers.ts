db1aed4d801a5157f56657ddb105084d9c9f4827
olehmell <oleh.melnechyk@gmail.com>
2022-03-18 16:45:43 +0200

    Remove test files

--- src/connections/loggers.ts
+++ src/connections/loggers.ts
@@ -1,7 +1,7 @@
 import { newLogger } from '@subsocial/utils'
 
-export const expressApiLog = newLogger('Express API', 'DEBUG')
-export const substrateLog = newLogger('Substrate', 'DEBUG')
-export const ipfsLog = newLogger('IPFS', 'DEBUG')
-export const postgesLog = newLogger('Postgres', 'DEBUG')
+export const expressApiLog = newLogger('Express API')
+export const substrateLog = newLogger('Substrate')
+export const ipfsLog = newLogger('IPFS')
+export const postgesLog = newLogger('Postgres')
 export const elasticLog = newLogger('ElasticSearch')
