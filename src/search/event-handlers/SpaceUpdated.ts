291bc983385a0ea2feaff985c134ceab9a38fb46
olehmell <oleh.melnechyk@gmail.com>
2022-05-05 20:41:33 +0300

    wip

--- src/search/event-handlers/SpaceUpdated.ts
+++ src/search/event-handlers/SpaceUpdated.ts
@@ -1,5 +1,5 @@
 import { SpaceId } from '@subsocial/types/substrate/interfaces';
-import { resolveSubsocialApi } from '../../connections';
+import { findSpace } from '../../substrate/api-wrappers';
 import { EventHandlerFn } from '../../substrate/types';
 import { indexSpaceContent } from '../indexer';
 
@@ -7,9 +7,7 @@ export const onSpaceUpdated: EventHandlerFn = async (eventAction) => {
   const { data } = eventAction;
   const spaceId = data[1] as SpaceId;
 
-  const { substrate } = await resolveSubsocialApi()
-
-  const space = await substrate.findSpace({ id: spaceId });
+  const space = await findSpace(spaceId);
   if (!space) return;
 
   await indexSpaceContent(space);
