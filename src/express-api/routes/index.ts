13d58456ba63869bdd861d5f23fba4466255778b
olehmell <oleh.melnechyk@gmail.com>
2022-11-15 13:10:06 +0200

    Add offchain route

--- src/express-api/routes/index.ts
+++ src/express-api/routes/index.ts
@@ -1,12 +1,14 @@
 import { Router } from "express"
 import createIpfsRoutes from "./ipfs"
 import createHealthRoutes from "./health"
+import createOffchainRoutes from "./offchain"
 
 export const createV1Routes = () => {
   const router = Router()
 
   router.use('/ipfs', createIpfsRoutes())
   router.use('/health', createHealthRoutes())
+  router.use('/offchain', createOffchainRoutes())
 
   return router
 }
