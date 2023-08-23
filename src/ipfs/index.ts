eb2be594dc8cc466396a2cb95a097365ff7b6caa
Mell <oleh.melnechyk@gmail.com>
2023-08-23 13:31:32 +0300

    Update for staging

--- src/ipfs/index.ts
+++ src/ipfs/index.ts
@@ -2,6 +2,7 @@ import { SubsocialIpfsApi } from '@subsocial/api'
 import {crustIpfsAuth, ipfsClusterUrl, ipfsNodeUrl} from "../env";
 function getIpfsApi() {
   const headers = crustIpfsAuth ? { authorization: `Bearer ${crustIpfsAuth}` } : {}
+  const props = crustIpfsAuth ? { asLink: false, 'meta.gatewayId': 1 } : { asLink: true }
 
   const ipfs = new SubsocialIpfsApi({
     ipfsNodeUrl,
@@ -15,12 +16,12 @@ function getIpfsApi() {
     ipfs,
     saveAndPinJson: async (content: Record<any, any>) => {
       const cid = await ipfs.saveJson(content)
-      await ipfs.pinContent(cid, { asLink: true, 'meta.gatewayId': 1 })
+      await ipfs.pinContent(cid, props)
       return cid
     },
     saveAndPinFile: async (content: any) => {
       const cid = await ipfs.saveFile(content)
-      await ipfs.pinContent(cid, { asLink: true, 'meta.gatewayId': 1 })
+      await ipfs.pinContent(cid, props)
       return cid
     },
   }
